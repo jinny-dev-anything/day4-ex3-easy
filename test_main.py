013df9a761555f4df6e1b3405c1e1ede6544c99f
jinny-dev-anything <jinny.dev.anything@gmail.com>
2026-08-07 15:12:56 +0900

    Add test_add2 for additional add function validation
    Added a new test for the add function with different inputs.

--- test_main.py
+++ test_main.py
@@ -4,5 +4,8 @@ from main import add, greet
 def test_add():
     assert add(1, 2) == 3
 
+def test_add2():
+    assert add(3, 4) == 4
+
 def test_greet():
     assert greet("Jenkins") == "Hello, Jenkins!"
