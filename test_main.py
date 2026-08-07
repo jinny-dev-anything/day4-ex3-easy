be803929706ca3f11750c9ce4f43c15a11abec3e
jinny-dev-anything <jinny.dev.anything@gmail.com>
2026-08-07 15:14:08 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,7 +5,7 @@ def test_add():
     assert add(1, 2) == 3
 
 def test_add2():
-    assert add(3, 4) == 4
+    assert add(3, 4) == 7
 
 def test_greet():
     assert greet("Jenkins") == "Hello, Jenkins!"
